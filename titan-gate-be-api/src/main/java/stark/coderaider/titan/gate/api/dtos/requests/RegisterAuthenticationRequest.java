952f584901f6f11ec024301eab77ea2026651d7e
DinoStark <15190304050@163.com>
2025-10-08 08:32:53 +0800

    Register

--- titan-gate-be-api/src/main/java/stark/coderaider/titan/gate/api/dtos/requests/RegisterAuthenticationRequest.java
+++ titan-gate-be-api/src/main/java/stark/coderaider/titan/gate/api/dtos/requests/RegisterAuthenticationRequest.java
@@ -2,8 +2,10 @@ package stark.coderaider.titan.gate.api.dtos.requests;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class RegisterAuthenticationRequest
+public class RegisterAuthenticationRequest implements Serializable
 {
     private String username;
     private String password;
