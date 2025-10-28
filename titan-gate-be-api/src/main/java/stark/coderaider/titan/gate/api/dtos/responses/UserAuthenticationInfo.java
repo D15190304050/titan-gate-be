b0af5cb53d143446f6afef38f262f7749914b1b2
DinoStark <15190304050@163.com>
2025-10-28 23:49:30 +0800

    UserAuthenticationInfo

--- titan-gate-be-api/src/main/java/stark/coderaider/titan/gate/api/dtos/responses/UserAuthenticationInfo.java
+++ titan-gate-be-api/src/main/java/stark/coderaider/titan/gate/api/dtos/responses/UserAuthenticationInfo.java
@@ -2,8 +2,10 @@ package stark.coderaider.titan.gate.api.dtos.responses;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class UserAuthenticationInfo
+public class UserAuthenticationInfo implements Serializable
 {
     private String username;
     private String email;
